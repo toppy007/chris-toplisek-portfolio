6b767e34ac2b144dd3d31e73521d0db90a54023e
toppy007 <chris_t72@hotmail.com>
2024-01-10 14:57:34 +0000

    styled the education section

--- src/components/feature.js
+++ src/components/feature.js
@@ -2,7 +2,7 @@ import * as React from "react"
 import { graphql } from "gatsby"
 import { VerticalTimelineElement }  from 'react-vertical-timeline-component';
 import 'react-vertical-timeline-component/style.min.css';
-import { Heading, Text, Logo, Box, Flex, NavLink, Kicker } from "./ui"
+import { Subhead, Text, Box, Flex, NavLink, Icon} from "./ui"
 
 export default function Feature(props) {
   console.log(props)
@@ -14,12 +14,14 @@ export default function Feature(props) {
         iconStyle={{ background: '#ff7f00', color: '#fff' }}
         >
         <Flex variant="start">
-          <Logo alt={props.alt} image={props.image.gatsbyImageData} size="medium"/> 
+          <Box center={true} width="quarter">
+          <Icon alt={props.alt} image={props.image.gatsbyImageData} size="Large"/> 
+          </Box>
           <Box >
-            <Heading>{props.heading}</Heading>
+            <Subhead >{props.heading}</Subhead>
             <Text variant="bold">{props.kicker}</Text>
             <Text>{props.year}</Text>
-            <Text> {props.about}</Text>
+            <Text>{props.about}</Text>
             <Text variant="bold"> {props.text}</Text>
             <Text>{props.links.text}</Text>
             {props.links &&
